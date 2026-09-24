f6758a1b1b7a5ebe9c2f4a849d4f82b7ba66895d
devARcoder18 <abdurrazzaqdeveloper562@gmail.com>
2026-09-24 10:11:48 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const http = require('http')
+const url = require('url')
+
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log("Error");
+
+//     console.log(data1)
+// })
+
+
+// server
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.decription);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+    pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.writeHead(200, {'Content-type': 'text/html'})
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
+
+
+    } else if (pathName === '/product'){
+         res.end("This is the Product Page")
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'})
+            res.end(data)
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello world'
+        })
+        res.end('<h1>Page Not Found</h1>')
+    }
+})
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log("Listening the request to port 8000")
+})
+
+
+
+// lecture 16 start continue
