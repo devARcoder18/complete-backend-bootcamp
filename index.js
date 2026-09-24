2b41b5b815292f4c8a2c52a927a33a9578b13293
devARcoder18 <abdurrazzaqdeveloper562@gmail.com>
2026-09-24 22:47:38 +0500

    lecture 16 - parsing variable url - completed

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    pathname = req.url;
-
     const { query, pathname} = url.parse(req.url, true)
 
     if(pathname === '/' || pathname === '/overview'){
@@ -48,7 +46,11 @@ const server = http.createServer((req, res) => {
 
 
     } else if (pathname === '/product'){
-         res.end("This is the Product Page")
+        res.writeHead(200, {'Content-type':'text/html'})
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product)
+         res.end(output)
+
     } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'})
             res.end(data)
