38b357068d06d3fcd6e754f88946fc47b61027f0
devARcoder18 <abdurrazzaqdeveloper562@gmail.com>
2026-09-25 16:04:56 +0500

    lecture 20 completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 const replaceTemplate = require('./modules/replaceTemplate')
+const slugify = require('slugify')
+
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 //     if (err) return console.log("Error");
 
@@ -17,6 +19,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true}))
+console.log(slugs)
 
 
 const server = http.createServer((req, res) => {
